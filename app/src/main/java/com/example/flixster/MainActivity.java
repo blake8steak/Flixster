bc98f92d7c04bae3d2ca64432233024739a1db25
Blake Herrera <blakeherrera@fb.com>
2021-06-24 18:16:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RatingBar;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.adapters.MovieAdapter;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String sampleUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //UI references
+        View mainView = findViewById(android.R.id.content);
+        ActionBar bar = getSupportActionBar();
+
+        //change color of ActionBar and view
+        bar.setBackgroundDrawable(new ColorDrawable(0x87110700));
+        mainView.setBackgroundColor(Color.DKGRAY);
+
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+        //set adapter on RecycleView
+        rvMovies.setAdapter(movieAdapter);
+        //set layout manager on RecycleView
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(sampleUrl, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception");
+                    e.printStackTrace();
+                }
+        }
+
+        @Override
+        public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+            Log.d(TAG, "onFailure");
+        }
+    });
+    }
+}
